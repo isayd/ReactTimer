174dfa78a1b87a6d97e1e74525ea3665916adecd
Isayd Gallegos Flores <isayd@isayd.local>
2017-03-09 22:35:54 -0800

    Initial commit

---
+++ app/components/Nav.jsx
@@ -0,0 +1,32 @@
+var React = require('react');
+var {Link, IndexLink} = require('react-router');
+
+
+var Nav = () => {
+    return (
+      <div className = "top-bar">
+        <div className = "top-bar-left">
+          <ul className = "menu">
+            <li className="menu-text">
+              React Time App
+            </li>
+            <li>
+              <IndexLink to ="/" activeClassName="active-link" activeStyle = {{fontWeight: 'bold'}}>Timer</IndexLink>
+            </li>
+            <li>
+              <Link to ="/countdown" activeClassName="active-link" activeStyle = {{fontWeight: 'bold'}}>Countdown</Link>
+            </li>
+          </ul>
+        </div>
+        <div className = "top-bar-right">
+          <ul className="menu">
+            <li className="menu-text">
+              Created by <a href="https://github.com/isayd" target="_blank">Isayd</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    );
+};
+
+module.exports = Nav;
---
+++ app/components/Timer.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var Timer = React.createClass ({
+
+  render: function(){
+    return (
+      <h1>Timer component!!</h1>
+    );
+  }
+
+});
+
+module.exports = Timer;
